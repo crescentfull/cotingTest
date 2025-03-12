8b0c0dd5e94676bcba1d57e36672d91a00e619d8
Yeongrok Song <78721108+crescentfull@users.noreply.github.com>
2025-03-12 17:37:04 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 9 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,11 +1,8 @@
 def solution(numbers):
-    answer = -1
-    check_numbers = [0,1,2,3,4,5,6,7,8,9]
-    number = 0
+    answer = 0
     
-    for check_number in check_numbers:
-        if check_number not in numbers:
-            number += check_number
+    for number in range(0,10):
+        if number not in numbers:
+            answer += number
             
-    answer = number
     return answer
