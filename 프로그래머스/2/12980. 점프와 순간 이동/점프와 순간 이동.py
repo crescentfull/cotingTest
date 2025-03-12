c8e5371cfa9c93aff81d6e767ee5e71b04053a91
Yeongrok Song <78721108+crescentfull@users.noreply.github.com>
2025-03-12 19:11:50 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,2 +1,13 @@
 def solution(n):
-    return bin(n).count('1')
+    answer = 0  # 사용한 건전지 양
+    
+    while n > 0:
+        if n % 2 == 0:  
+            n //= 2  # 순간이동
+        else:
+            answer += 1  # 점프
+            n -= 1  # 점프 후 순간이동 가능하도록 변경
+    
+    return answer
+# def solution(n):
+#     return bin(n).count('1')
