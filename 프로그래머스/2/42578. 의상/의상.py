bcacd9c483b02fcb502f214fe0e425934e92e52a
Yeongrok Song <78721108+crescentfull@users.noreply.github.com>
2025-03-20 16:10:09 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,29 +1,21 @@
 def solution(clothes):
-    # 종류별로 의상의 수를 세기 위한 딕셔너리
     category_dict = {}
     
     for name, category in clothes:
-        # 각 의상의 종류(category)를 key로 하여 개수를 센다.
         if category in category_dict:
             category_dict[category] += 1
         else:
             category_dict[category] = 1
-    
-    # 모든 종류를 곱셈하기 위한 초기값
-    # (각 종류별 경우의 수: 의상 개수 + 1)
-    # 최종적으로 (카테고리1의 경우의 수) * (카테고리2의 경우의 수) * ... - 1
-    # -1은 모든 종류에서 아무것도 고르지 않은 경우를 제외하기 위함
+            
     result = 1
     
-    for category, count in category_dict.items():
-        # 해당 카테고리의 경우의 수 = count(의상 수) + 1(아무것도 안 고름)
-        result *= (count + 1)
+    print(category_dict)
     
-    # 아무것도 고르지 않은 경우를 제외
+    for category, count in category_dict.items():
+        result *= (count + 1)
+        
     result -= 1
     
+    print(result)
+    
     return result
-
-# 테스트
-print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]])) # 예제1: 5
-print(solution([["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]))           # 예제2: 3
