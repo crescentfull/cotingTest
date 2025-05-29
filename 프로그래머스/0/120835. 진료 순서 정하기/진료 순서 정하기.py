905e30ad95aac82b35d3b54bf2999b672cb3155e
Yeongrok Song <78721108+crescentfull@users.noreply.github.com>
2025-05-29 22:18:51 +0900

    Update 진료 순서 정하기.py

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,8 +1,7 @@
 def solution(emergency):
-    answer = []
-    
+    # 딕셔너리로 값을 매겨주는데, 매개변수 값을 역순으로 정열하여 인덱스 값을 튜플로 뽑아주는 enumerate를 이용
+    # (0,76),(1,24),(2,3)
+    # 이렇게 반환 되니까 본래 key: value를 바꿔서 등록해줘야한다.(입력값과 같은 값의 우선순위를 뽑아줘야하니까)
     rank = {value: num+1 for num, value in enumerate(sorted(emergency, reverse=True))}
     
-    answer = [rank[i] for i in emergency]
-        
-    return answer
+    return [rank[i] for i in emergency]
