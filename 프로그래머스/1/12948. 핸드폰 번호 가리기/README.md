e797bffaba18a33b12ccc34b816d8dc9775b3ed7
Yeongrok Song <78721108+crescentfull@users.noreply.github.com>
2025-06-09 16:18:56 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/README.md
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 핸드폰 번호 가리기 - 12948 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12948?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12948) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 9.18 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 16일 20:37:57
+2025년 06월 09일 16:18:52
 
 ### 문제 설명
 
--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,15 +1,8 @@
 def solution(phone_number):
     answer = ''
-    # 전화번호의 길이를 구함
-    length = len(phone_number)
-    
-    # 전화번호의 뒷 4자리를 제외한 나머지를 '*'로 변환
-    masked_part = '*' * (length - 4)
-    
-    # 전화번호의 뒷 4자리
-    visible_part = phone_number[-4:]
-    
-    # 두 부분을 결합하여 answer 변수에 저장
-    answer = masked_part + visible_part
-    
+    back_digit = phone_number[-4::]
+    front_digit = phone_number[:-4]
+    new_front_digit = "*"*len(front_digit)
+    answer = new_front_digit + back_digit
+    print(answer)
     return answer
