9ea730d0ba12605ab460de6a3ac82d25565167d4
Yeongrok Song <78721108+crescentfull@users.noreply.github.com>
2025-09-29 19:30:23 +0900

    Update 원소들의 곱과 합.py

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,10 +1,22 @@
-from functools import reduce
-
-def multi(x,y):
-    return x*y
+from math import prod
 
 def solution(num_list):
     answer = 0
-    if reduce(multi, num_list) < sum(num_list)**2:
-        return  1
+    if prod(num_list) < sum(num_list)**2:
+        return 1
     return answer
+
+
+# 1.첫번째
+# from functools import reduce
+
+# def multi(x,y):
+#     return x*y
+
+# def solution(num_list):
+#     answer = 0
+    
+#     if reduce(multi, num_list) < sum(num_list)**2:
+#         return 1
+    
+#     return answer
