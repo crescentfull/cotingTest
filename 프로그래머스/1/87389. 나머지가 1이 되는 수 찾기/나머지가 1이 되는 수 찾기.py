8ee986bd865da8ea572fd58d59d39c48dea45252
Yeongrok Song <78721108+crescentfull@users.noreply.github.com>
2025-10-29 23:57:21 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 75.17 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
@@ -1,12 +1,10 @@
 def solution(n):
     answer = 0
     
-    # n을 x로 나누면 나머지가 1이 되야한다.
     for x in range(1, n+1):
         if n % x == 1:
-            return x
+            answer = x
             break
-    
-    # n을 x로 나누었을때 가장작은 숫자가 반횐되어야 한다.
+        
     
     return answer
